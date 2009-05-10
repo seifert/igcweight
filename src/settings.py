916c637df6ae2a27d8319fd8c09a9ac6dc286daa
Jan Seifert <jan.seifert@fotkyzcest.net>
2009-05-10 11:57:46 +0000

    Parse command line options (get working directory)

--- src/settings.py
+++ src/settings.py
@@ -2,22 +2,34 @@
 
 from os import mkdir
 from os.path import abspath, dirname, join, expanduser, isdir
+from sys import argv
+from getopt import getopt
 from decimal import Decimal
 
 DEBUG = True
 
+HOME_DIR = None
+__optlist, __args = getopt( argv[1:], 'd:' )
+for __o, __v in __optlist:
+    if __o == '-d':
+        HOME_DIR = abspath(__v)
+        break
+
 try:
     from win32com.shell import shellcon, shell
-    USER_DIR = shell.SHGetFolderPath(0, shellcon.CSIDL_APPDATA, 0, 0)
+    USER_DIR = shell.SHGetFolderPath( 0, shellcon.CSIDL_APPDATA, 0, 0 )
 except ImportError:
     USER_DIR = expanduser("~")
 
 LAST_OPEN_FILE_PATH = USER_DIR
 
-HOME_DIR   = join( USER_DIR, 'igcweight-08' )
+HOME_DIR   = HOME_DIR == None and join( USER_DIR, 'igcweight-08' ) or HOME_DIR
 PHOTOS_DIR = join( HOME_DIR, 'photos' )
 BASE_DIR   = abspath( join(dirname(__file__), '..') )
 IMAGES_DIR = join( BASE_DIR, 'images' )
+
+print HOME_DIR
+aaa
 
 if not isdir(HOME_DIR):
     mkdir(HOME_DIR)
