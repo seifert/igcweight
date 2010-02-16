917c9487d9b56fa1e4d2836d25715d022687121c
Jan Seifert <jan.seifert@fotkyzcest.net>
2010-02-16 10:29:39 +0100

    igcweight - refactoring

--- src/igcweight.py
+++ src/igcweight.py
@@ -3,38 +3,41 @@
 
 " Main program module "
 
-from os.path import join
-
 def main():
     " main() - start application "
-    
+
     import wx
     app = wx.App()
 
     try:
-        from settings import BASE_DIR
-        
+        # Init locales
+        from settings import LOCALE_DIR
+
         mylocale = wx.Locale(wx.LANGUAGE_DEFAULT)
-        mylocale.AddCatalogLookupPathPrefix( join(BASE_DIR, 'locale') )
+        mylocale.AddCatalogLookupPathPrefix(LOCALE_DIR)
         mylocale.AddCatalog('igcweight')
+        # MS Windows hack
         mylocale.AddCatalog('wxstd')
-        
-        wx.InitAllImageHandlers()
-    
+
+        # Init main window and application
         from gui_main import Main
+
         main = Main(None, -1, "")
         app.SetTopWindow(main)
         main.Show()
-    
+
         app.SetAppName( main.GetTitle() )
         app.MainLoop()
-        
+
     except Exception, e:
         from wx import GetTranslation as _
 
-        message = "%s\n\n%s" % ( _("Some error during starting application"),
-                                 unicode(str(e), 'utf-8') )
+        message = u"%s\n\n%s" % (
+            _("Some error during starting application"),
+            unicode( str(e), 'utf-8' )
+        )
         wx.MessageBox( message, _("Error"), wx.OK | wx.ICON_ERROR, None )
+
         raise
 
 if __name__ == '__main__':
