63cd73a69a547e87ae8a2e59c4a48819939f823f
Jan Seifert <jan.seifert@fotkyzcest.net>
2010-05-23 22:00:49 +0200

    default exception handler

--- src/igcweight.py
+++ src/igcweight.py
@@ -3,42 +3,57 @@
 
 " Main program module "
 
+import sys
+import traceback
+
+import wx
+
 def main():
     " main() - start application "
-
-    import wx
+    # Init wx app
     app = wx.App()
 
+    # Set default exception handler
+    sys.excepthook = _excepthook
+
+    # Init locales
+    from settings import LOCALE_DIR
+    mylocale = wx.Locale(wx.LANGUAGE_DEFAULT)
+    mylocale.AddCatalogLookupPathPrefix(LOCALE_DIR)
+    mylocale.AddCatalog('igcweight')
+    # MS Windows hack
+    mylocale.AddCatalog('wxstd')
+
+    # Init main window and application
+    from gui_main import Main
+    main = Main(None, -1, "")
+    app.SetAppName( main.GetTitle() )
+    app.SetTopWindow(main)
+    main.Show()
+
+    app.MainLoop()
+
+def _excepthook(etype, value, tb):
+    " Process unhandled exception "
     try:
-        # Init locales
-        from settings import LOCALE_DIR
-
-        mylocale = wx.Locale(wx.LANGUAGE_DEFAULT)
-        mylocale.AddCatalogLookupPathPrefix(LOCALE_DIR)
-        mylocale.AddCatalog('igcweight')
-        # MS Windows hack
-        mylocale.AddCatalog('wxstd')
-
-        # Init main window and application
-        from gui_main import Main
-
-        main = Main(None, -1, "")
-        app.SetTopWindow(main)
-        main.Show()
-
-        app.SetAppName( main.GetTitle() )
-        app.MainLoop()
-
-    except Exception, e:
         from wx import GetTranslation as _
+    except:
+        _ = lambda s: s
+    try:
+        from settings import DEBUG
+    except:
+        DEBUG = True
 
-        message = u"%s\n\n%s" % (
-            _("Some error during starting application"),
-            unicode( str(e), 'utf-8' )
-        )
-        wx.MessageBox( message, _("Error"), wx.OK | wx.ICON_ERROR, None )
+    if DEBUG:
+        tb_str = u"".join(traceback.format_exception(etype, value, tb))
+        message = u"%s\n\n%s" % (value, tb_str)
+    else:
+        message = unicode(value)
 
-        raise
+    wx.MessageBox(message, _("IGC Weight error"), wx.OK | wx.ICON_ERROR, None)
+
+    if DEBUG:
+        sys.__excepthook__(etype, value, tb)
 
 if __name__ == '__main__':
     main()
