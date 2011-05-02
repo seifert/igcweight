75e519a9936801e67e4d9241b1ea8c56d9a799b7
Jan Seifert <jan.seifert@centrumholdings.com>
2011-05-02 21:16:45 +0200

    settings - version 0.9.6

--- src/settings.py
+++ src/settings.py
@@ -7,7 +7,7 @@ from getopt import getopt
 
 from configuration import Configuration
 
-VERSION_APP = (0, 9, 5)
+VERSION_APP = (0, 9, 6)
 VERSION_DB  = (0, 9)
 VERSION_GIT = ""
 VERSION = ".".join(str(s) for s in VERSION_APP)
